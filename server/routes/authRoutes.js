07cfaa577008211aa99b37432fe8949ba6d3f1a3
Ayush Vyas <work.ayushvyas17@gmail.com>
2025-02-11 12:24:24 +0530

    added api for get user details

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { generateOTP, signup, login, verifyAndLogin } from "../controllers/auth.js";
+import { generateOTP, signup, login, verifyAndLogin , getUser } from "../controllers/auth.js";
 import verifyOTP from "../middlewares/verifyOtp.js";
 
 const router = express.Router();
@@ -8,5 +8,6 @@ router.post("/generate-otp", generateOTP);
 router.post("/signup", verifyOTP, signup);
 router.post("/login/email", login);
 router.post("/login/phone", verifyOTP, verifyAndLogin);
+router.post("/user",getUser);
 
 export default router;
